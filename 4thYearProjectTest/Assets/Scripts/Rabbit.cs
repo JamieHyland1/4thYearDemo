4d7022d9486215e43df7548757a631d45e4eb4b8
JamieHyland1 <jamiehyland01@gmail.com>
2019-11-23 15:15:47 +0000

    Added rabbit.cs script

---
+++ 4thYearProjectTest/Assets/Scripts/Rabbit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rabbit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+   public float hunger;
+
+    [SerializeField]
+    //how much do we want to decrease his hunger level each step
+    float hungerStep;
+    void Start()
+    {
+        hunger = 100f;
+        hungerStep = 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hunger > 0) hunger -= hungerStep;
+    }
+}
